ec65d8110847602165a5f4ffbd5e9a7a55853c2c
YourBrainEatsYou <jnzwicker@gmail.com>
2021-10-01 11:00:27 +0200

    🪤 told the app to inject the mock cashier API instead of the real API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {AbstractCashierApiService} from './api/cashier/abstract-cashier-api.service';
+import {MockCashierApiService} from './api/cashier/mock-cashier-api.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +15,9 @@ import {AppStoreModule} from './store/app-store.module';
     BrowserModule,
     AppRoutingModule,
     AppStoreModule
+  ],
+  providers: [
+    { provide: AbstractCashierApiService, useClass: MockCashierApiService }
   ],
   bootstrap: [AppComponent]
 })
