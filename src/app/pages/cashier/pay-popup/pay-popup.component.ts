5fd619530f404efc380ca7264f9171222e467a3d
YourBrainEatsYou <me@yourbraineatsyou.ch>
2022-03-31 01:37:19 +0200

    fixed double emit of order

--- src/app/pages/cashier/pay-popup/pay-popup.component.ts
+++ src/app/pages/cashier/pay-popup/pay-popup.component.ts
@@ -1,12 +1,10 @@
 import { AbstractCashierApiService } from "@/api/cashier/abstract-cashier-api.service";
-import { OrderDto } from "@/interfaces/dto/order-dto";
-import { OrderType } from "@/interfaces/enum/order-type";
 import { WebUsbService } from "@/services/web-usb/web-usb.service";
 import { CartFacadeService } from "@/store/cart/cart-facade.service";
 import { UserInterfaceFacadeService } from "@/store/user-interface/user-interface-facade.service";
 import { HttpErrorResponse } from "@angular/common/http";
 import { Component, HostListener, ElementRef, OnDestroy } from '@angular/core';
-import { Subject, takeUntil, tap, EMPTY, of } from "rxjs";
+import { Subject, takeUntil, tap, EMPTY, of, take, delay } from "rxjs";
 import { first, catchError } from "rxjs/operators";
 
 @Component({
@@ -14,7 +12,7 @@ import { first, catchError } from "rxjs/operators";
   templateUrl: './pay-popup.component.html',
   styleUrls: ['./pay-popup.component.scss']
 })
-export class PayPopupComponent implements OnDestroy{
+export class PayPopupComponent implements OnDestroy {
 
   cart$ = this.cartFacadeService.cart$;
   total$ = this.cartFacadeService.total$;
@@ -27,7 +25,7 @@ export class PayPopupComponent implements OnDestroy{
     private cartFacadeService: CartFacadeService,
     private elementRef: ElementRef,
     private webUsbService: WebUsbService,
-    private cashierApiService: AbstractCashierApiService,
+    private cashierApiService: AbstractCashierApiService
   ) {
   }
 
@@ -40,27 +38,30 @@ export class PayPopupComponent implements OnDestroy{
     this.webUsbService.sendMessage('Badge vor leser halten');
 
     this.webUsbService.messages$.pipe(
-      takeUntil(this.destroyed$)
-    ).subscribe((badgeId: string) => {
-      this.webUsbService.sendMessage(`${'Guthaben wird'.padEnd(16, ' ')}abgefragt...`);
-      this.placeOrder(badgeId);
-    })
+      take(1),
+      tap((badgeId) => {
+        this.webUsbService.sendMessage(`${ 'Guthaben wird'.padEnd(16, ' ') }abgefragt...`);
+        console.log('here');
+       this.placeOrder(badgeId);
+      })
+    ).subscribe();
   }
 
   @HostListener('click', ['$event.target'])
   onClick(target: EventTarget): void {
-    if(target === this.elementRef.nativeElement){
+    if (target === this.elementRef.nativeElement) {
       this.userInterfaceFacadeService.hidePaymentPopup();
     }
   }
 
   @HostListener('document:keydown.escape')
-  onKeypress(): void{
+  onKeypress(): void {
     this.userInterfaceFacadeService.hidePaymentPopup();
   }
 
-  private placeOrder(badgeId: string){
+  private placeOrder(badgeId: string) {
     this.orderProducts$.pipe(
+      first(),
       takeUntil(this.destroyed$)
     ).subscribe((products) => {
       this.cashierApiService.placeOrder({
@@ -68,8 +69,15 @@ export class PayPopupComponent implements OnDestroy{
         products
       }).pipe(
         first(),
-        tap(() => this.userInterfaceFacadeService.hidePaymentPopup()),
         tap(() => this.cartFacadeService.reset()),
+        tap(() => this.userInterfaceFacadeService.hidePaymentPopup()),
+        delay(250),
+        tap((payload) => {
+          this.webUsbService.sendMessage(`${ 'Neues Guthaben:'.padEnd(16, ' ') }${ ('CHF ' + (payload.data.balance / 100).toFixed(2)).padEnd(16, ' ') }`);
+          setTimeout(() => {
+            this.webUsbService.defaultMessage();
+          }, 2000);
+        }),
         catchError((error: HttpErrorResponse) => {
           console.error(error);
           return of(EMPTY);
